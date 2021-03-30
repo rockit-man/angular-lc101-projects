568fd23364df6b2cc41bef3aef56b1c2aaca2ec8
rockit-man <demetrius.esq@gmail.com>
2021-03-30 16:09:40 -0500

    'exercise complete'

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Imbibe & Vibe w/the Tribe';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://wallpapercave.com/wp/wp5517299.jpg';
+  image3 = 'http://pilerats.com/assets/Uploads/tyler-the-creator-tamale.jpg';
 
   constructor() { }
 
